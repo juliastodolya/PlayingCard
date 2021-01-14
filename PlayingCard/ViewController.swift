924edef98e8896ca6e416a6378651fcf1be83118
Julia Stodolya <y.stodolya@gmail.com>
2021-01-14 17:53:33 +0300

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Юлия on 14.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
